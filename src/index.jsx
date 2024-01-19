20bd23493a1dad2dd34a7e508bf700b2ec25c726
Viery Nugroho <viery15102002@gmail.com>
2024-01-19 10:15:03 +0700

    fix route

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,13 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 import './styles/style.css';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-	<StrictMode>
-		<RouterProvider router={router}>
-			<App />
-		</RouterProvider>
-	</StrictMode>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
